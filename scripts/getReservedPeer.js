d39b95251ea74eb64334a2ad997a08e3dd95e9e0
Jorge Mederos <jmederosalvarado@gmail.com>
2024-02-14 16:30:15 +0100

    logging

--- scripts/getReservedPeer.js
+++ scripts/getReservedPeer.js
@@ -38,6 +38,7 @@ async function main() {
     } catch(e) {
       if (i <= maxAttempts) {
         await sleep(500);
+        console.log(`Attempt ${i} failed. Retrying...`);
       } else {
         console.log(`Failed to get enode URL after ${maxAttempts} attempts. Exiting. Error: ${e}`);
       }
